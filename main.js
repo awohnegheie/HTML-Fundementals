dff1ceddab94e3f65bd7189645d5381de506d23b
Awoh! <48054640+awohnegheie@users.noreply.github.com>
2019-03-13 15:16:34 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Learning JavaScript</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+  <div class="header">
+     <h1>Learning JavaScript</h1>
+    <p>Awoh Negheie</p>
+    
+<div>
+  
+<div class="container">
+
+</div>
+
+<script>
+
+</script>
+
+</body>
+</html>
